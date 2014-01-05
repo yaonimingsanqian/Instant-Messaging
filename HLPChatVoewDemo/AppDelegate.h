a0458dd7e882d7214cf00f9a9b4ece8f5cabbbf8
yaonimingsanqian <zhaogwhebust@gmail.com>
2014-01-05 11:01:39 +0800

    Initial Commit

---
+++ HLPChatVoewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HLPChatVoewDemo
+//
+//  Created by zhao on 14-1-5.
+//  Copyright (c) 2014年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
